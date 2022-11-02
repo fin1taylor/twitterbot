494042eec5486b5c1a62d861f1eca7af0352196e
fin1taylor <36804778+fin1taylor@users.noreply.github.com>
2022-11-02 17:32:28 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         '5xKRnzO9jJX1tJ7AhqElOThuy',
+  consumer_secret:      'QEVXNNoParsFv16C2714vpTgmPOpKoAce84pmkpOHzTw4SJbOU',
+  access_token:         '1586820652274749440-Ea23U0g0qFMeGYEi0vXi2QJoRxijeL',
+  access_token_secret:  '2kRCgwko2tCWJmu7KklAohkqIpRCNJyqy0CravQ5lMzip'
+}
---
+++ queerCowboy.js
@@ -0,0 +1,147 @@
+
+// DEBUG
+var debug = true;		// if we don't want it to post to Twitter! Useful for debugging!
+
+// Twitter Essentials
+// Twitter Library
+var Twit = require('twit');
+
+const request = require('request')
+
+// Include configuration file
+var T = new Twit(require('./config.js'));
+
+// Helper function for arrays, picks a random thing
+Array.prototype.pick = function() {
+	return this[Math.floor(Math.random()*this.length)];
+}
+Array.prototype.remove = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+// // Wordnik stuff
+// function nounUrl(minCorpusCount, limit) {
+// 	return "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun&minCorpusCount=" + minCorpusCount + "&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&limit=" + limit + "&api_key=" + WordnikAPIKey;
+// }
+
+function getQuote() {
+	return "https://api.quotable.io/random?tags=self%7Ccharacter%7Chappiness%7Cinspirational%7Clove";
+}
+
+// Post a status update
+function tweet() {
+
+	con.setRequestMethod("GET");	
+	// var tweetText = pre.pick();
+
+	if(debug) 
+		console.log('Debug mode: ', tweetText);
+	else
+		T.post('statuses/update', {status: tweetText }, function (err, reply) {
+			if (err != null){
+				console.log('Error: ', err);
+			}
+			else {
+				console.log('Tweeted: ', tweetText);
+			}
+		});
+}
+
+function followAMentioner() {
+	T.get('statuses/mentions_timeline', { count:50, include_rts:1 },  function (err, reply) {
+		  if (err !== null) {
+			console.log('Error: ', err);
+		  }
+		  else {
+		  	var sn = reply.pick().user.screen_name;
+			if (debug) 
+				console.log(sn);
+			else {
+				//Now follow that user
+				T.post('friendships/create', {screen_name: sn }, function (err, reply) {
+					if (err !== null) {
+						console.log('Error: ', err);
+					}
+					else {
+						console.log('Followed: ' + sn);
+					}
+				});
+			}
+		}
+	});
+}
+
+function respondToMention() {
+	T.get('statuses/mentions_timeline', { count:100, include_rts:0 },  function (err, reply) {
+		  if (err !== null) {
+			console.log('Error: ', err);
+		  }
+		  else {
+		  	mention = reply.pick();
+		  	mentionId = mention.id_str;
+		  	mentioner = '@' + mention.user.screen_name;
+		  	
+		  	var tweet = mentioner + " " + pre.pick();
+			if (debug) 
+				console.log(tweet);
+			else
+				T.post('statuses/update', {status: tweet, in_reply_to_status_id: mentionId }, function(err, reply) {
+					if (err !== null) {
+						console.log('Error: ', err);
+					}
+					else {
+						console.log('Tweeted: ', tweet);
+					}
+				});
+		  }
+	});
+}
+
+function runBot() {
+	console.log(" "); // just for legible logs
+	var d=new Date();
+	var ds = d.toLocaleDateString() + " " + d.toLocaleTimeString();
+	console.log(ds);  // date/time of the request	
+
+	request(getQuote(), function(err, response, data) {
+		if (err != null) return; // bail if no data
+		console.log(data);
+		var quote = [];
+		var pairs = data.split(",");
+		for (var i = 1; i < pairs.length; i++) {
+			var pair = pairs[i];
+			var keyValue = pair.split(":");
+			quote[keyValue[0].replaceAll("\"", "")] = keyValue[1].replaceAll("\"", "");
+			console.log(keyValue[0].replaceAll("\"", ""), keyValue[1].replaceAll("\"", ""))
+		}
+		
+		///----- NOW DO THE BOT STUFF
+		// var rand = Math.random();
+
+ 		// if(rand <= 1.60) {      
+		// 	console.log("-------Tweet something");
+		// 	tweet();
+			
+		// } else if (rand <= 0.80) {
+		// 	console.log("-------Tweet something @someone");
+		// 	respondToMention();
+			
+		// } else {
+		// 	console.log("-------Follow someone who @-mentioned us");
+		// 	followAMentioner();
+		// }
+	});
+}
+
+// Run the bot
+runBot();
+
+// And recycle every hour
+setInterval(runBot, 1000 * 60 * 60);
